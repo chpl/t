0365b76e92b6db99df1009099cab52674970413f
ChaimP <chaim.platonov@env0.com>
2024-10-28 15:08:23 +0200

    ansible test

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -x
 
-source /opt/global_python_venv/bin/activate
-pip3 install --user -r requirements.txt
+python3 -m venv $HOME/ansible
+source $HOME/ansible/bin/activate
+pip3 install -r requirements.txt
 ansible-galaxy collection install community.docker:3.10.4 --force
 ansible-galaxy collection install ansible.legacy --force
