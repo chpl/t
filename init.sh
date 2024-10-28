45a3f5e503a0bb67ca985445194346188a95cdfb
ChaimP <chaim.platonov@env0.com>
2024-10-28 13:49:47 +0200

    ansible test

--- init.sh
+++ init.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-# Display public IP
-curl ifconfig.me
-
-# Display Terraform version
-terraform version
+set -x
 
 source /opt/global_python_venv/bin/activate
 pip3 install --user -r requirements.txt
