a0ec374c4ebf5d0c0c3ca83be03036ab57fd9779
ChaimP <chaim.platonov@env0.com>
2025-01-09 14:15:58 +0200

    provider alias in module

--- module/main.tf
+++ module/main.tf
@@ -5,20 +5,23 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
-      configuration_aliases = [
-        "aws.this",
-      ]
     }
   }
+}
+
+provider "aws" {
+  alias  = "this"
 }
 
 variable "name_suffix" {}
 
 resource "aws_s3_bucket" "chaim-multi-region" {
+  provider = aws.this
   bucket = "chaim-multi-region-${var.name_suffix}"
 }
 
 resource "aws_iam_role" "chaim-multi-region" {
+  provider = aws.this
   name = "chaim-multi-region-${var.name_suffix}"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
