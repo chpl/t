8330749a72cd87038e122069d2c2feaf307d3014
Chaim Platonov <chaim.platonov@env0.com>
2025-04-20 19:27:42 +0300

    tg run all

---
+++ module2/terragrunt.hcl
@@ -0,0 +1,7 @@
+terraform {
+  source = "../shared"
+}
+
+inputs = {
+  prefix = "module2"
+}
---
+++ shared/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "prefix" {}
+
+module "s3_bucket_external" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "chaim-delete-me-${var.prefix}-my-s3-bucket"
+  acl    = "private"
+  versioning = {
+    enabled = true
+  }
+}
