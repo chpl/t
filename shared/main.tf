519bb90aaa6e3c16cea292106eaeb570671168f1
Chaim Platonov <chaim.platonov@env0.com>
2025-04-21 10:15:53 +0300

    tg run all

--- shared/main.tf
+++ shared/main.tf
@@ -19,7 +19,6 @@ module "s3_bucket_external" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "chaim-delete-me-${var.prefix}-my-s3-bucket"
-  acl    = "private"
   versioning = {
     enabled = true
   }
