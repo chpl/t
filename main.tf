7f5ee92b94c419796c9b488495d45b56f13a1a96
Chaim Platonov <chaim.platonov@env0.com>
2025-04-29 16:55:15 +0300

    outputs

--- main.tf
+++ main.tf
@@ -1,6 +1,24 @@
-resource "null_resource" "null" {
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.3"
+    }
+  }
 }
+
+variable "input" {}
+
+resource "random_pet" "this" {}
 
 output "test" {
   value = "chaim"
+}
+
+output "pet_name" {
+  value = random_pet.this.id
+}
+
+output "from_input" {
+  value = var.input
 }
