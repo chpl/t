bb2e2f62854aaf79970452d8dae40860845fc31e
Chaim Platonov <chaim.platonov@env0.com>
2025-05-05 10:31:06 +0300

    many outputs

--- main.tf
+++ main.tf
@@ -7,18 +7,130 @@ terraform {
   }
 }
 
-variable "input" {}
-
 resource "random_pet" "this" {}
 
-output "test" {
-  value = "chaim"
+
+
+output "test_0" {
+  value = "${random_pet.this.id}_0"
 }
 
-output "pet_name" {
-  value = random_pet.this.id
+output "test_1" {
+  value = "${random_pet.this.id}_001"
 }
 
-output "from_input" {
-  value = var.input
+output "test_2" {
+    value = "${random_pet.this.id}_002"
+}
+
+output "test_3" {
+    value = "${random_pet.this.id}_003"
+}
+
+output "test_4" {
+    value = "${random_pet.this.id}_004"
+}
+
+output "test_5" {
+    value = "${random_pet.this.id}_005"
+}
+
+output "test_6" {
+    value = "${random_pet.this.id}_006"
+}
+
+output "test_7" {
+    value = "${random_pet.this.id}_007"
+}
+
+output "test_8" {
+    value = "${random_pet.this.id}_008"
+}
+
+output "test_9" {
+    value = "${random_pet.this.id}_009"
+}
+
+output "test_10" {
+    value = "${random_pet.this.id}_010"
+}
+
+output "test_11" {
+    value = "${random_pet.this.id}_011"
+}
+
+output "test_12" {
+    value = "${random_pet.this.id}_012"
+}
+
+output "test_13" {
+    value = "${random_pet.this.id}_013"
+}
+
+output "test_14" {
+    value = "${random_pet.this.id}_014"
+}
+
+output "test_15" {
+    value = "${random_pet.this.id}_015"
+}
+
+output "test_16" {
+    value = "${random_pet.this.id}_016"
+}
+
+output "test_17" {
+    value = "${random_pet.this.id}_017"
+}
+
+output "test_18" {
+    value = "${random_pet.this.id}_018"
+}
+
+output "test_19" {
+    value = "${random_pet.this.id}_019"
+}
+
+output "test_20" {
+    value = "${random_pet.this.id}_020"
+}
+
+output "test_21" {
+    value = "${random_pet.this.id}_021"
+}
+
+output "test_22" {
+    value = "${random_pet.this.id}_022"
+}
+
+output "test_23" {
+    value = "${random_pet.this.id}_023"
+}
+
+output "test_24" {
+    value = "${random_pet.this.id}_024"
+}
+
+output "test_25" {
+    value = "${random_pet.this.id}_025"
+}
+
+output "test_26" {
+    value = "${random_pet.this.id}_026"
+}
+
+output "test_27" {
+    value = "${random_pet.this.id}_027"
+}
+
+output "test_28" {
+    value = "${random_pet.this.id}_028"
+}
+
+output "test_29" {
+    value = "${random_pet.this.id}_029"
+}
+
+output "test_30" {
+    value = "${random_pet.this.id}_030"
 }
