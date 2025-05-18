a0a8c0824409ee68a0104db3574224fa9f1b01a7
Chaim Platonov <chaim.platonov@env0.com>
2025-05-18 12:49:14 +0300

    acm example

--- main.tf
+++ main.tf
@@ -1,6 +1,24 @@
-resource "null_resource" "null" {
+terraform {
+  required_version = ">= 1.0.0" # Ensure that the Terraform version is 1.0.0 or higher
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws" # Specify the source of the AWS provider
+      version = "~> 5.0"        # Use a version of the AWS provider that is compatible with version
+    }
+  }
 }
 
-output "test" {
-  value = "chaim"
+provider "aws" {
+  region = "us-east-1" # Set the AWS region to US East (N. Virginia)
 }
+
+resource "aws_acm_certificate" "import" {
+  certificate_body  = var.certificate_body
+  certificate_chain = var.certificate_chain
+  private_key       = var.private_key
+}
+
+variable "certificate_body" {}
+variable "certificate_chain" {}
+variable "private_key" {}
