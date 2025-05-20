8ea06c54c791d051b654460447f4d63acffef5d6
Chaim Platonov <chaim.platonov@env0.com>
2025-05-20 12:52:02 +0300

    providers

--- main.tf
+++ main.tf
@@ -1,6 +1,72 @@
-resource "null_resource" "null" {
-}
+terraform {
+  required_version = ">= 1.0.0" # Ensure that the Terraform version is 1.0.0 or higher
 
-output "test" {
-  value = "chaim"
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+    }
+    github = {
+      source = "integrations/github"
+    }
+
+    mongodb = {
+      source = "mongodb/mongodbatlas"
+    }
+
+    azapi = {
+      source = "azure/azapi"
+    }
+
+    okta = {
+      source = "okta/okta"
+    }
+
+    databricks = {
+      source = "databricks/databricks"
+    }
+
+    sonarqube = {
+      source = "jdamata/sonarqube"
+    }
+
+    cosmo = {
+      source = "wundergraph/cosmo"
+    }
+
+    datadog = {
+      source = "datadog/datadog"
+    }
+
+    vault = {
+      source = "hashicorp/vault"
+    }
+
+    flux = {
+      source = "fluxcd/flux"
+    }
+
+    lastpass = {
+      source = "nrkno/lastpass"
+    }
+
+    sops = {
+      source = "carlpett/sops"
+    }
+
+    tailscale = {
+      source = "tailscale/tailscale"
+    }
+
+    tfe = {
+      source = "hashicorp/tfe"
+    }
+
+    grafana = {
+      source = "grafana/grafana"
+    }
+
+    onepassword = {
+      source = "1password/onepassword"
+    }
+  }
 }
