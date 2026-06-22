3e197bde008de897bac8496d68d5897556d9c3d8
chpl <chaim@platonov.me>
2026-06-22 15:48:24 +0300

    loop in provisioner

--- main.tf
+++ main.tf
@@ -1,2 +1,5 @@
 resource "null_resource" "null" {
+  provisioner "local-exec" {
+    command = "for i in $(seq 1 3000); do echo \"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain # $i at $(date +%T.%N)\"; sleep 0.05; done"
+  }
 }
