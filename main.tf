178b14ed940e8b91d86386528adf13ddc428a65c
chpl <chaim@platonov.me>
2026-08-06 15:30:45 +0300

    always recreate the resource

--- main.tf
+++ main.tf
@@ -13,6 +13,10 @@ locals {
 }
 
 resource "null_resource" "null" {
+  triggers = {
+    always = uuid()
+  }
+
   provisioner "local-exec" {
     command = "for i in $(seq 1 ${local.iterations}); do echo \"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain # $i at $(date +%T.%N)\"; sleep ${var.sleep_interval_seconds}; done"
   }
